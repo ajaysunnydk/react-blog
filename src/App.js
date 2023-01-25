b5c2ff674568be567289f1b7fe541c1d1747e9db
ajaysunnydk <ajaysunnydk167@gmail.com>
2023-01-25 18:38:02 +0530

    added map

--- src/App.js
+++ src/App.js
@@ -11,56 +11,179 @@ import cricket_image from './components/images/cricket.jpeg'
 import corona_image from './components/images/corona.jpeg'
 import default_image from './components/images/default.jpeg'
 
+import { useState } from 'react';
+
 
 function App() {
 
   let card = [
     {
-      title:"Crypto",
-      images:blog_image
+      title: "Crypto",
+      images: default_image
     },
     {
-      title:"ChatGPT",
-      images:default_image
+      title: "ChatGPT",
+      images: blog_image
     },
     {
-      title:"Corona",
-      images:corona_image
+      title: "Corona",
+      images: corona_image
     },
     {
-      title:"Ind Vs Nz",
-      images:cricket_image
+      title: "Ind Vs Nz",
+      images: cricket_image
     },
     {
-      title:"Default",
-      images:blog_image
+      title: "Default",
+      images: default_image
     },
   ];
+
+  var a = "IND vs NZ";
+  var [theme, setTheme] = useState("light")
+
+  const change = () => {
+    if (theme == "light")
+      setTheme("dark");
+    else
+      setTheme("light");
+  }
+
+  var [score, setScore] = useState(0);
+  var [wickets, setWickets] = useState(0);
+  var [balls, setBalls] = useState(0);
+  var [matchStatus, setMatchStatus] = useState("Match Started");
+
+  const six = () => {
+    if (balls > 11) {
+      setBalls(12);
+      setScore(score);
+      setMatchStatus("Match Ended");
+    }
+    else if (wickets > 9) {
+      setWickets(10);
+      setScore(score);
+      setMatchStatus("Match Ended");
+    }
+    else {
+      setBalls(balls + 1);
+      setScore(score + 6);
+    }
+  }
+  const four = () => {
+    if (balls > 11) {
+      setBalls(12);
+      setScore(score);
+      setMatchStatus("Match Ended");
+    }
+    else if (wickets > 9) {
+      setWickets(10);
+      setScore(score);
+      setMatchStatus("Match Ended");
+    }
+    else {
+      setBalls(balls + 1);
+      setScore(score + 4);
+    }
+  }
+  const three = () => {
+    if (balls > 11) {
+      setBalls(12);
+      setScore(score);
+      setMatchStatus("Match Ended");
+    }
+    else if (wickets > 9) {
+      setWickets(10);
+      setScore(score);
+      setMatchStatus("Match Ended");
+    }
+    else {
+      setBalls(balls + 1);
+      setScore(score + 3);
+    }
+  }
+  const two = () => {
+    if (balls > 11) {
+      setBalls(12);
+      setScore(score);
+      setMatchStatus("Match Ended");
+    }
+    else if (wickets > 9) {
+      setWickets(10);
+      setScore(score);
+      setMatchStatus("Match Ended");
+    }
+    else {
+      setBalls(balls + 1);
+      setScore(score + 2);
+    }
+
+  }
+  const one = () => {
+    if (balls > 11) {
+      setBalls(12);
+      setScore(score);
+      setMatchStatus("Match Ended");
+    }
+    else if (wickets > 9) {
+      setWickets(10);
+      setScore(score);
+      setMatchStatus("Match Ended");
+    }
+    else {
+      setBalls(balls + 1);
+      setScore(score + 1);
+    }
+
+  }
+  const out = () => {
+    if (balls > 11) {
+      setBalls(12);
+      setScore(score);
+      setMatchStatus("Match Ended");
+    }
+    else if (wickets > 9) {
+      setWickets(10);
+      setScore(score);
+      setMatchStatus("Match Ended");
+    }
+    else {
+      setBalls(balls + 1);
+      setWickets(wickets + 1);
+    }
+
+
+  }
 
   return (
     <div className="App">
       <div className="main-ctn">
         <Header></Header>
+        {/* <div className={theme}>
+          <h1>{a}</h1>
+          <h3>IND : {score}/{wickets}</h3>
+          <h4>Balls : {balls}</h4>
+          <div>
+            <button onClick={six} >6</button>
+            <button onClick={four} >4</button>
+            <button onClick={three} >3</button>
+            <button onClick={two} >2</button>
+            <button onClick={one} >1</button>
+          </div>
+          <button onClick={out} >Out</button>
+          <h6>{matchStatus}</h6>
+        </div> */}
         <div className="content-ctn">
-          <Practice ite="veg"></Practice>
-          <Practice ite="nonVeg"></Practice>
-          <Practice ite="nonVeg"></Practice>
-          <Practice ite="veg"></Practice>
-          <Practice ite="veg"></Practice>
-          <Practice ite="nonVeg"></Practice>
-          <Practice ite="nonVeg"></Practice>
-          <Practice ite="veg"></Practice>
-          <Practice ite="veg"></Practice>
-          <Card title={card[0].title} imageid={card[0].images}></Card>
-          <Card title={card[1].title} imageid={card[1].images}></Card>
-          <Card title={card[2].title} imageid={card[2].images}></Card>
-          <Card title={card[3].title} imageid={card[3].images}></Card>
-          <Card title={card[3].title} imageid={card[3].images}></Card>
-          <Card title={card[3].title} imageid={card[3].images}></Card>
+          {/* <Practice ite="veg"></Practice> */}
+          {
+            card.map(v => (
+              <Card title={v.title} imageid={v.images}></Card>
+            ))
+          }
         </div>
         <ContactUs></ContactUs>
       </div>
-           
+
     </div>
   );
 }
--- src/components/Practice.js
+++ src/components/Practice.js
@@ -1,10 +1,24 @@
 import './css/practice.css'
+import  {useState} from 'react'
+
 function Practice(props){
+
+    var a = 5;
+    const fun = ()=>{
+        console.log("Clicked - Veg");
+        a=10;
+    }
+
+    
 
     const item = props.ite;
     if(item=="veg"){
         return(
             <div className="veg">
+                <button onClick={fun} >Click Here</button>
+                <div>
+                    {a}
+                </div>
             </div>
         )
     }
